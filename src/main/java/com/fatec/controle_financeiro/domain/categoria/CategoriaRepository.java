2c6806ebe92d99674198c42098a77596e4e39c21
GabRodrigues23 <gabrigues23@gmail.com>
2024-11-28 22:07:21 -0300

    Revert "Implementation of Category"
    This reverts commit c189d7b2032bb43aace7ba5a6b02943a3433c8a7.

--- src/main/java/com/fatec/controle_financeiro/domain/categoria/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.controle_financeiro.domain.categoria;
-import com.fatec.controle_financeiro.entities.Categoria;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    Optional<Categoria> findByDescricao(String descricao);
- }
